d51a01e61bf7db19fdd6f0a59402e70fc41c1a6d
Jenya Proviz <114678330+jenyaproviz@users.noreply.github.com>
2022-12-15 13:15:38 +0200

    Add files via upload

--- scriptCart.js
+++ scriptCart.js
@@ -1,52 +1,128 @@
-// add to cart //
-
 let products = [
-  ['picture1', 100],
-  ['picture2', 200],
-  ['picture3', 300],
-]
-let shoppingCart = []
+  ["item1", 100],
+  ["item2", 200],
+  ["item3", 300],
+];
 
-function addToCart1(item) {
-  let newDiv = document.createElement('div')
-  let txtItem = document.createTextNode(
-    'you added picture 1' +
-      ' ' +
-      'to the cart' +
-      ' the price is' +
-      ' ' +
-      products[0][1],
-  )
+//מוסיפים מוצרים לסל
 
-  newDiv.appendChild(txtItem)
-  document.body.appendChild(newDiv)
+function addItemOne() {
+  let newDiv = document.createElement("div");
+
+  let textNode = document.createTextNode(
+    "The product is " + products[0][0] + " The price is " + products[0][1]
+  );
+  newDiv.appendChild(textNode);
+  document.body.appendChild(newDiv);
+  newDiv.id = "itemOne";
 }
-function addToCart2(item) {
-  let newDiv = document.createElement('div')
-  let txtItem = document.createTextNode(
-    'you added picture 2' +
-      ' ' +
-      'to the cart' +
-      ' the price is' +
-      ' ' +
-      products[1][1],
-  )
 
-  newDiv.appendChild(txtItem)
-  document.body.appendChild(newDiv)
+function addItemTwo() {
+  let newDiv = document.createElement("div");
+  let textNode = document.createTextNode(
+    "The product is " + products[1][0] + " The price is " + products[1][1]
+  );
+  newDiv.appendChild(textNode);
+  document.body.appendChild(newDiv);
+  newDiv.id = "itemTwo";
 }
-function addToCart3(item) {
-  let newDiv = document.createElement('div')
-  let txtItem = document.createTextNode(
-    'you added picture 3' +
-      ' ' +
-      'to the cart' +
-      ' the price is' +
-      ' ' +
-      products[2][1],
-  )
 
-  newDiv.appendChild(txtItem)
-  document.body.appendChild(newDiv)
+function addItemThree() {
+  let newDiv = document.createElement("div");
+  let textNode = document.createTextNode(
+    "The product is " + products[2][0] + " The price is " + products[2][1]
+  );
+  newDiv.appendChild(textNode);
+  document.body.appendChild(newDiv);
+  newDiv.id = "itemThree";
 }
-// remove //
+
+// מורידים מוצרים מהסל
+function removeItemOne() {
+  let element = document.getElementById("itemOne");
+  element.remove();
+}
+function removeItemTwo() {
+  let element = document.getElementById("itemTwo");
+  element.remove();
+}
+function removeItemThree() {
+  let element = document.getElementById("itemThree");
+  element.remove();
+}
+
+function checkout() {
+  let element = document.getElementById("checkout");
+  element.remove();
+}
+//______________add buttons counter____________________//
+
+let btnOne = document.querySelector("#btnAddOne");
+let countBtnOne = 0;
+
+btnOne.addEventListener("click", function () {
+  countBtnOne += 1;
+  console.log(countBtnOne);
+});
+
+let btnTwo = document.querySelector("#btnAddTwo");
+let countBtnTwo = 0;
+
+btnTwo.addEventListener("click", function () {
+  countBtnTwo += 1;
+  console.log(countBtnTwo);
+});
+
+let btnThree = document.querySelector("#btnAddThree");
+let countBtnThree = 0;
+
+btnThree.addEventListener("click", function () {
+  countBtnThree += 1;
+  console.log(countBtnThree);
+});
+// ___________remove buttons counter_______________//
+
+let btnOneRem = document.querySelector("#btnRemoveOne");
+let countBtnOneRem = 0;
+
+btnOneRem.addEventListener("click", function () {
+  countBtnOneRem += 1;
+  console.log(countBtnOneRem);
+});
+
+let btnTwoRem = document.querySelector("#btnRemoveTwo");
+let countBtnTwoRem = 0;
+
+btnTwoRem.addEventListener("click", function () {
+  countBtnTwoRem += 1;
+  console.log(countBtnTwoRem);
+});
+
+let btnThreeRem = document.querySelector("#btnRemoveThree");
+let countBtnThreeRem = 0;
+
+btnThreeRem.addEventListener("click", function () {
+  countBtnThreeRem += 1;
+  console.log(countBtnThreeRem);
+});
+//__________function total___________________//
+function order(total) {
+  let totalOne = (countBtnOne - countBtnOneRem) * products[0][1];
+  let totalTwo = (countBtnTwo - countBtnTwoRem) * products[1][1];
+  let totalThree = (countBtnThree - countBtnThreeRem) * products[2][1];
+  total = totalOne + totalTwo + totalThree;
+
+  let newDiv = document.createElement("div");
+  let textNode = document.createTextNode(
+    "The total price for your cart is " + total
+  );
+  newDiv.appendChild(textNode);
+  document.body.appendChild(newDiv);
+
+  console.log(total);
+  newDiv.id = "checkout";
+}
+// _____________refresh page____________//
+function refreshPage() {
+  window.location.reload();
+}
